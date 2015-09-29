24698c8f9d713be00df1fd7226fff80b214aebac
Jessamyn Smith <geekchick77@gmail.com>
2015-09-28 20:39:49 -0400

    Added delete option for scraping

--- codementor/management/commands/scrape_codementor_reviews.py
+++ codementor/management/commands/scrape_codementor_reviews.py
@@ -1,4 +1,5 @@
 import dateutil.parser
+from optparse import make_option
 from scrapy.spiders import Spider
 from scrapy.crawler import CrawlerProcess
 
@@ -27,7 +28,8 @@ class ReviewSpider(Spider):
                                                                        date=review_date,
                                                                        content=review_content)
             if existing_reviews.count() == 0:
-                reviewer = codementor_models.Client(name=reviewer_name)
+                # This will group reviews by people with the same display name
+                reviewer, _ = codementor_models.Client.objects.get_or_create(name=reviewer_name)
                 reviewer.save()
                 review = codementor_models.Review(reviewer=reviewer,
                                                   date=review_date,
@@ -38,7 +40,17 @@ class ReviewSpider(Spider):
 class Command(NoArgsCommand):
     help = 'Scrape reviews from codementor and store locally'
 
+    option_list = NoArgsCommand.option_list + (
+        make_option('--delete', action='store_true', dest='delete', default=False,
+                    help='Delete data before scraping.'),
+    )
+
     def handle(self, *args, **options):
+        delete = options.get('delete')
+        if delete:
+            codementor_models.Client.objects.filter(reviews__isnull=False).delete()
+            codementor_models.Review.objects.all().delete()
+
         process = CrawlerProcess({
             'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
         })
--- codementor/models.py
+++ codementor/models.py
@@ -4,14 +4,20 @@ from django.db import models
 class Client(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        ordering = ['name']
+
     def __str__(self):
         return self.name
 
 
 class Review(models.Model):
-    reviewer = models.ForeignKey(Client)
+    reviewer = models.ForeignKey(Client, related_name='reviews')
     date = models.DateField()
     content = models.TextField()
 
+    class Meta:
+        ordering = ['-date']
+
     def __str__(self):
-        return '%s (%s)' % (self.name, self.date)
+        return '%s (%s)' % (self.reviewer.name, self.date)
