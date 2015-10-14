58c23dbad432c5c6d1cf990024d5afc111c6b4f8
Jessamyn Smith <geekchick77@gmail.com>
2015-10-14 10:12:59 -0600

    Added Totals and Averages for all numeric fields in Payment and Payout

--- codementor/admin.py
+++ codementor/admin.py
@@ -11,40 +11,50 @@ class ReviewAdmin(admin.ModelAdmin):
     search_fields = ['reviewer__name', 'content']
 
 
-class PayoutAdmin(admin.ModelAdmin):
-    list_display = ['date', 'method', 'amount', 'total_earnings']
-    list_filter = ['method']
+class SumAverageChangeList(ChangeList):
+    """Custom total rows at bottom of changelist"""
 
+    NUMERIC_FIELDS = ('Integer', 'Decimal number')
 
-class PaymentChangeList(ChangeList):
-    """Custom total rows at bottom of changlist"""
+    def get_field(self, field_name):
+        for field in self.model._meta.fields:
+            if field_name == field.attname:
+                return field
+        return None
 
-    def get_total_payments(self, queryset):
-        """ Payment total for changelist"""
-        total = models.Payment()
-        total.__unicode__ = lambda: u"Total Payments"
-        earning_dict = queryset.aggregate(Sum('earnings'))
-        total.earnings = earning_dict.items()[0][1]
+    def get_aggregate(self, queryset, title, aggregate_function):
+        """ Get aggregate data for changelist"""
+        total = self.model()
+        total.__unicode__ = lambda: title
+        for field_name in self.list_display:
+            field = self.get_field(field_name)
+            if field and not field.primary_key:
+                if field.choices:
+                    setattr(total, field_name, None)
+                elif str(field.description) in self.NUMERIC_FIELDS:
+                    result_data = queryset.aggregate(aggregate_function(field_name))
+                    setattr(total, field_name, result_data.items()[0][1])
         return total
 
-    def get_average_payment(self, queryset):
-        """ Average payment for changelist"""
-        avg = models.Payment()
-        avg.__unicode__ = lambda: u"Average cost"
-        earning_dict = queryset.aggregate(Avg('earnings'))
-        avg.earnings = earning_dict.items()[0][1]
-        return avg
-
     def get_results(self, request):
-        super(PaymentChangeList, self).get_results(request)
+        super(SumAverageChangeList, self).get_results(request)
         queryset = self.get_queryset(request)
-        total_payments = self.get_total_payments(queryset)
-        average_payment = self.get_average_payment(queryset)
+        total = self.get_aggregate(queryset, u"Totals", Sum)
+        average = self.get_aggregate(queryset, u"Averages", Avg)
         # HACK: in order to get the objects loaded we need to call for
         # queryset results once so simple len function does it
         len(self.result_list)
-        self.result_list._result_cache.append(total_payments)
-        self.result_list._result_cache.append(average_payment)
+        self.result_list._result_cache.append(total)
+        self.result_list._result_cache.append(average)
+
+
+class PayoutAdmin(admin.ModelAdmin):
+    list_display = ['date', 'method', 'amount', 'total_earnings']
+    list_filter = ['method', 'date']
+
+    def get_changelist(self, request, **kwargs):
+        """Override the default changelist"""
+        return SumAverageChangeList
 
 
 class PaymentAdmin(admin.ModelAdmin):
@@ -54,7 +64,7 @@ class PaymentAdmin(admin.ModelAdmin):
 
     def get_changelist(self, request, **kwargs):
         """Override the default changelist"""
-        return PaymentChangeList
+        return SumAverageChangeList
 
 
 admin.site.register(models.Client)
