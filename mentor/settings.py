ac6b47916dc7dcf15c807904bd40039c38f81ba9
Jessamyn Smith <geekchick77@gmail.com>
2016-05-30 12:24:10 -0400

    Removed settings from flake8 excludes

--- mentor/settings.py
+++ mentor/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'xg!y@2rg#*5(wfa0i^hasj^x7(@%7+o&f5a+j)y*d@nah-vpzt')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
+                            'xg!y@2rg#*5(wfa0i^hasj^x7(@%7+o&f5a+j)y*d@nah-vpzt')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
