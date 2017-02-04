3246c31a7a96ad91ac399be47b9c791df3b86663
Jessamyn Smith <geekchick77@gmail.com>
2017-02-04 08:26:21 -0700

    Added amount earned by next bonus level to print status

--- codementor/utils.py
+++ codementor/utils.py
@@ -1,3 +1,5 @@
+from decimal import Decimal, ROUND_DOWN
+
 from django.db.models import Sum
 
 from codementor import models as codementor_models
@@ -7,7 +9,10 @@ def format_status():
     separator = '------------------------------'
     payments = codementor_models.Payment.objects.all()
     pending_total = payments.filter(payout__isnull=True).aggregate(Sum('earnings'))['earnings__sum']
-    payments_to_date = 'Payments to date: $%s' % pending_total
+    payments_to_date = 'Payments to date: ${}'.format(pending_total)
     weekly_rating = codementor_models.WeeklyRating.objects.all().order_by('-week_end').first()
-    status = '\n'.join([separator, payments_to_date, str(weekly_rating), separator])
+    payment_increase = pending_total * Decimal("0.025")
+    payment_increase = payment_increase.quantize(Decimal('.01'), rounding=ROUND_DOWN)
+    amount_gained = 'Amount gained from next level: ${}'.format(payment_increase)
+    status = '\n'.join([separator, payments_to_date, str(weekly_rating), amount_gained, separator])
     return status
